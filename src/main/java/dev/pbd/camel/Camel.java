f9618a006bb502fb39a06e76a28d3a89a8b1f155
Prabodh Agarwal <prabodh1194@gmail.com>
2022-11-26 02:19:10 +0530

    fix how camel sqs will function

--- src/main/java/dev/pbd/camel/Camel.java
+++ src/main/java/dev/pbd/camel/Camel.java
@@ -1,4 +1,20 @@
 package dev.pbd.camel;
 
-public class Camel {
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.endpoint.StaticEndpointBuilders;
+
+
+public class Camel extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        var url = StaticEndpointBuilders.aws2Sqs("test_test");
+        from(url).process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                System.out.println(exchange.getMessage().getBody().toString());
+            }
+        });
+    }
 }
