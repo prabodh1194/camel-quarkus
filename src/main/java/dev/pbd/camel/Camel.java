1418559a696fbf0d8e56843f7ec83566e0d67a49
Prabodh Agarwal <prabodh1194@gmail.com>
2022-11-26 09:48:24 +0530

    fetch queue name from props

--- src/main/java/dev/pbd/camel/Camel.java
+++ src/main/java/dev/pbd/camel/Camel.java
@@ -1,7 +1,5 @@
 package dev.pbd.camel;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.endpoint.StaticEndpointBuilders;
 
@@ -9,7 +7,7 @@ import org.apache.camel.builder.endpoint.StaticEndpointBuilders;
 public class Camel extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        var url = StaticEndpointBuilders.aws2Sqs("test_test");
-        from(url).to("file:/tmp/example");
+        var url = StaticEndpointBuilders.aws2Sqs("{{pbd.queue}}");
+        from(url).to("log:example");
     }
 }
