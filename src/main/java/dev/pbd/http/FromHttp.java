76f44eadd02be58825120e1752217c836cb5a881
Prabodh Agarwal <prabodh1194@gmail.com>
2022-11-26 20:54:46 +0530

    typo

--- src/main/java/dev/pbd/http/FromHttp.java
+++ src/main/java/dev/pbd/http/FromHttp.java
@@ -7,7 +7,7 @@ public class FromHttp extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("netty-http://http://localhost:8888/greeting")
-                .log("recieved a request")
+                .log("received a request")
                 .setBody(simple("[${date:now:yyyy-MM-dd'T'HH:mm:ss.SSSZ}] Hello, world!"));
     }
 }
