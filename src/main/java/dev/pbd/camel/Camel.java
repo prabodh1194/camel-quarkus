78d6c90fc0c6a95eb8b3b02bda2e16573c2825e6
Prabodh Agarwal <prabodh1194@gmail.com>
2022-11-26 10:03:46 +0530

    inject prop

--- src/main/java/dev/pbd/camel/Camel.java
+++ src/main/java/dev/pbd/camel/Camel.java
@@ -2,11 +2,19 @@ package dev.pbd.camel;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.endpoint.StaticEndpointBuilders;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
 
 
+@ApplicationScoped
 public class Camel extends RouteBuilder {
+    @ConfigProperty(name = "pbd.queue")
+    String message;
+
     @Override
     public void configure() throws Exception {
+        System.out.println(message);
         var url = StaticEndpointBuilders.aws2Sqs("{{pbd.queue}}");
         from(url).to("log:example");
     }
