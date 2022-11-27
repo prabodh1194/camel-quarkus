15c2ae31825177a95688195f463495cf4a9a9744
Prabodh Agarwal <prabodh1194@gmail.com>
2022-11-27 18:26:34 +0530

    simplify pipe creation

--- src/main/java/dev/pbd/http/FromHttp.java
+++ src/main/java/dev/pbd/http/FromHttp.java
@@ -1,6 +1,9 @@
 package dev.pbd.http;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.netty.http.NettyHttpMessage;
 
 public class FromHttp extends RouteBuilder {
 
@@ -8,9 +11,18 @@ public class FromHttp extends RouteBuilder {
     public void configure() throws Exception {
         restConfiguration().component("netty-http").host("localhost").port("8888");
 
-        rest("/greeting").get().to("direct:greeting");
+        from("rest://get:greeting:/{me}/{lang}")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        var httpMsg = exchange.getMessage(NettyHttpMessage.class);
+                        var hdrs = httpMsg.getHeaders();
 
-        from("direct:greeting")
+                        log.info((String) hdrs.get("me"));
+                        log.info((String) hdrs.get("lang"));
+                        log.info((String) hdrs.getOrDefault("z", 10));
+                    }
+                })
                 .log("received a request")
                 .setBody(simple("[${date:now:yyyy-MM-dd'T'HH:mm:ss.SSSZ}] Hello, world!"));
     }
